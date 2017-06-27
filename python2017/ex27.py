f80e24ba567f593282482cad2afe6e2bd46a0a7c
timothyseguiawork <timothyseguiawork@gmail.com>
2017-06-27 12:18:06 -0400

    Exercise 27: Memorizing Logic

---
+++ python2017/ex27.py
@@ -0,0 +1,57 @@
+# Exercise 27: Memorizing Logic
+# 06/27/17
+
+# This is simple basic logic that makes real programs work and that real
+# programmers need every day
+
+# Do this for an entire week!
+
+# and
+# or
+# not
+# != not equal
+# == equal
+# >= greater than or equal
+# <= less than or equal
+# True
+# False
+
+# NOT       |     True?
+# not False      True
+# not True       False
+
+# OR            |      True?
+# True or False       True
+# True or True        True
+# False or True       True
+# False or False      False
+
+# NOT OR              |      True?
+# NOT (True or False)       False
+# NOT (True or True)        False
+# NOT (False or True)       False
+# NOT (False or False)      True
+
+# AND           |      True?
+# True or False       False
+# True or True        True
+# False or True       False
+# False or False      False
+
+# NOT AND             |       True?
+# NOT (True and False)       True
+# NOT (True and True)        False
+# NOT (False and True)       True
+# NOT (False and False)      True
+
+# !=     |    True?
+# 1 != 0     True
+# 1 != 1     False
+# 0 != 1     True
+# 0 != 0     False
+
+# ==     |    True?
+# 1 == 0     False
+# 1 == 1     True
+# 0 == 1     False
+# 0 == 1     True
