adb38a1873343737f7159373549862cbfc9523d6
timothyseguiawork <timothyseguiawork@gmail.com>
2017-07-20 07:34:06 -0400

    Changes

---
+++ java2017/chapter1/chapter1.java
@@ -0,0 +1,32 @@
+public class chapter1{
+    public static void main(String[] args){
+        System.out.println("1.2 Commands");
+        //This command prints the string or int given inside the brackets on the screen.
+        //ln is a suffix for the word line
+        System.out.println("System.out.println(\"Hello world!\")");
+        System.out.println("This command prints: Hello World!\n");
+
+        
+        //1.3 Compiler and Interpreter
+        /*
+        * Computers do not directly understand the programming language we are using.
+        * We need a compiler between the source code and the computer.
+        */
+
+        
+        System.out.println("1.4 Components of Commands\n");
+        
+        System.out.println("1.4.1 Semicolon, a semicolon ';' is used to separate different commands");
+        System.out.println("System.out.print(\"Hello \"); System.out.print(\"world\");");
+        System.out.println("System.out.print(\"!\")");
+        System.out.print("Hello "); System.out.print("world");
+        System.out.print("!\n\n");
+        
+        System.out.println("1.4.2 Parameters (Information Passed to Commands)");
+        System.out.println("Information passed by a command are the parameters of a command.");
+        System.out.println("System.out.println(\"Hello\"); in this command \"Hello\" is the parameter\n");
+        
+        System.out.println("1.4.3 Comments");
+        System.out.println("Comments are a useful way to make notes in the source code for yourself and others\n");
+    } 
+}
--- java2017/programs/src/ex1.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Timot on 7/18/2017.
- */
-public class ex1 {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
